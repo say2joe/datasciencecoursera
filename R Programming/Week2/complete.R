03027a0eda066a356a6b4e86f3db898f89dc03a9
Joe Johnson <jjohnson@marketshare.com>
2015-02-01 13:55:28 -0800

    updates

--- R Programming/Week2/complete.R
+++ R Programming/Week2/complete.R
@@ -1,15 +1,24 @@
 complete <- function(directory, id = 1:332) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
+  pwd <- getwd()
+  setwd(directory)
 
-  ## 'id' is an integer vector indicating the monitor ID numbers
-  ## to be used
+  ## 'id' is an integer vector indicating the monitor ID numbers to be used
+  data <- data.frame()
+
+  for (file in id) {
+    filedata <- read.csv(paste(sprintf('%03d', file), '.csv', sep=''))
+    data <- rbind(data, cbind(file, nrow(filedata[complete.cases(filedata),])))
+  }
 
   ## Return a data frame of the form:
   ## id nobs
   ## 1  117
   ## 2  1041
   ## ...
-  ## where 'id' is the monitor ID number and 'nobs' is the
-  ## number of complete cases
+  ## where 'id' is the monitor ID number and 'nobs' is the number of complete cases
+  colnames(data) <- c('id', 'nobs')
+  setwd(pwd)
+  data
 }
--- R Programming/Week2/corr.R
+++ R Programming/Week2/corr.R
@@ -1,11 +1,26 @@
 corr <- function(directory, threshold = 0) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
+  pwd <- getwd()
+  setwd(directory)
 
   ## 'threshold' is a numeric vector of length 1 indicating the
   ## number of completely observed observations (on all
   ## variables) required to compute the correlation between
   ## nitrate and sulfate; the default is 0
+  data <- numeric(0)
+
+  for (file in list.files(pattern = ".csv")) {
+    filedata <- read.csv(file)
+    completeCases <- nrow(filedata[complete.cases(filedata),])
+    if (completeCases > threshold) {
+      data <- c(data, cor(filedata['nitrate'], filedata['sulfate'], use="complete.obs"))
+    }
+    rm(filedata)
+  }
+
+  setwd(pwd)
 
   ## Return a numeric vector of correlations
+  as.numeric(data)
 }
